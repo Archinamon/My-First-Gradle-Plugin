4d72dab0b7bf8cf1d9436d5f9ebebaa4530b23f0
archinamon <archinamon@yandex.ru>
2018-08-16 22:43:06 +0300

    simple task

--- buildSrc/src/main/java/ru/yandex/example/gradle/ReportPlugin.kt
+++ buildSrc/src/main/java/ru/yandex/example/gradle/ReportPlugin.kt
@@ -2,16 +2,22 @@ package ru.yandex.example.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.creating
+import org.gradle.kotlin.dsl.getValue
 
 class ReportPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.logger.error("Wow! Our custom plugin configured!")
 
-        project.task("preBuildReport").configure<Task> {
+        val preBuildReport by project.tasks.creating {
+            doFirst {
+                logger.error("This task launch!")
+            }
 
+            doLast {
+                logger.error("This task finished!")
+            }
         }
     }
 }
