90f9e1956a88d79ba9fd58b7d9373b379e2e1bd2
archinamon <archinamon@yandex.ru>
2018-08-23 20:13:27 +0300

    added constants

--- buildSrc/src/main/java/ru/yandex/example/gradle/TelegramEngine.kt
+++ buildSrc/src/main/java/ru/yandex/example/gradle/TelegramEngine.kt
@@ -1,4 +1,13 @@
 package ru.yandex.example.gradle
 
-class TelegramEngine {
+class TelegramEngine(private val token: String) {
+
+    private fun cleanup(report: String) = report.replace(UNDERSCORE_SYMBOL, DASH_SYMBOL)
+
+    private companion object {
+        const val API_HOST = "https://api.telegram.org"
+
+        const val UNDERSCORE_SYMBOL = "_"
+        const val DASH_SYMBOL = "-"
+    }
 }
